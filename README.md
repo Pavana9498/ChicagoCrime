0889cb8857da133de8b669e1b8018b78f76567f4
Pavana9498 <56892877+Pavana9498@users.noreply.github.com>
2021-04-03 17:06:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### How to run this project
+
+**Install Dependencies**
+1.  `npm install` to install all dependencies
+
+**Run the project**
+1. `npm run dev`  to run client with React
+
+
+## Screenshots
+### Client:
+![client-login](https://github.com/markkos89/qliksense/blob/main/readme/login-page.png?raw=true)
+![client-main-page](https://github.com/markkos89/qliksense/blob/main/readme/main-page.png?raw=true)
---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div style={{ height: "100vh" }}>
+        <iframe
+          title="All the stats"
+          width="100%"
+          height="100%"
+          style={{ "border-width": "0px" }}
+          src="https://sngqkved6b5w668.us.qlikcloud.com/single/?appid=981b5259-46df-4666-af3e-3fa79a3e53f9&sheet=206b0ccf-960f-4177-9a86-af4c117a36be&opt=ctxmenu,currsel"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
